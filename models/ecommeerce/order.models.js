3603cb17868e6708e71354277fe2a20cbc17de31
sohanthink <sohansourab@gmail.com>
2024-04-10 17:38:34 +0600

    ecommerce data modeliing complete

--- models/ecommeerce/order.models.js
+++ models/ecommeerce/order.models.js
@@ -1,1 +1,38 @@
 import mongoose from 'mongoose';
+
+
+const orderItemSchema = new mongoose.Schema({
+    quantity:{
+        type:Number,
+        required: true
+    },
+    productId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:Product
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice:{
+        type: Number,
+        required:true
+    },
+    customer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: User
+    },
+    orderItems:{
+        type:[orderItemSchema]
+    },
+    address:{
+        type: String,
+        required:true
+    },
+    status:{
+        type: String,
+        enum:['PENDING', 'DELIVERED', 'CANCELED'],
+        default:"PENDING"
+    },
+},{timestamps:true})
+
+export const Order = mongoose.model("Order", orderSchema)
